25abd122697374ac5d25c59fa4e4d5bd2868ee69
emersonr02 <254676587+emersonr02@users.noreply.github.com>
2026-05-05 23:14:08 +0100

    Add dbContext / Models Transferencia

---
+++ Data/DataFutDbContext.cs
@@ -0,0 +1,35 @@
+﻿using DataFut.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataFut.Data
+{
+    public class DataFutDbContext : DbContext
+    {
+        public DataFutDbContext(DbContextOptions<DataFutDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Clube> Clubes { get; set; }
+        public DbSet<Jogador> Jogadores { get; set; }
+        public DbSet<Posicao> Posicoes { get; set; }
+        public DbSet<Transferencia> Transferencias { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configuração da relação Muitos-para-Muitos com entidade explícita
+            modelBuilder.Entity<Transferencia>()
+                .HasKey(t => new { t.JogadorId, t.ClubeDestinoId, t.DataTransferencia, t.ClubeOrigemId }); // Chave composta para histórico
+
+            modelBuilder.Entity<Transferencia>()
+                .HasOne(t => t.Jogador)
+                .WithMany(j => j.Id)
+                .HasForeignKey(t => t.JogadorId);
+
+            modelBuilder.Entity<Transferencia>()
+                .HasOne(t => t.ClubeDestino)
+                .WithOne(c => c.Jogador)
+                .HasForeignKey(t => t.ClubeDestinoId);
+
+
+        }
+    }
+}
