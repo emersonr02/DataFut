28a88fc0fa426670ac2f12cbfce8b1572878e8d3
emersonr02 <254676587+emersonr02@users.noreply.github.com>
2026-05-05 22:22:49 +0100

    Add Models (Clube, Jogador, Posicao)

---
+++ Models/Entities/Posicao.cs
@@ -0,0 +1,11 @@
+﻿namespace DataFut.Models.Entities
+{
+    public class Posicao
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        // Relacionamento com Jogador
+        public List<Jogador> Jogadores { get; set; }
+    }
+}
