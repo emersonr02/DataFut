b6a18d9e1d3cda5f2a3803ae92a3cc02d1f5aa6d
codebycs <codebycs@gmail.com>
2026-05-08 22:14:39 +0100

    Fix: metodo GET jogadoresController

--- Controllers/JogadoresController.cs
+++ Controllers/JogadoresController.cs
@@ -3,6 +3,7 @@ using DataFut.Data;
 using DataFut.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using DataFut.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DataFut.Controllers
 {
@@ -62,7 +63,7 @@ namespace DataFut.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> Contratar()
         {
             var viewModel = new JogadorContratacaoViewModel
             {
@@ -71,11 +72,15 @@ namespace DataFut.Controllers
                     await _context.Clubes.OrderBy(c =>c.Nome).ToListAsync(),
                     "Id",
                     "Nome"
-                 )
+                 ),
+                Posicoes = new SelectList(
+                    await _context.Posicoes.OrderBy(p => p.Nome).ToListAsync(),
+                    "Id",
+                    "Nome"
+        )
             };
 
             return View(viewModel);
         }
-
     }
 }
